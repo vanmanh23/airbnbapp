bcc976fdf9ce0e05597fff3e8eebc8e825acef9e
manhnguyen <manhcan03@gmail.com>
2023-12-11 09:22:42 +0700

    continue fetch api room

--- apps/server/src/modules/detailroom/rooms.controler.ts
+++ apps/server/src/modules/detailroom/rooms.controler.ts
@@ -3,7 +3,7 @@ import { roomsService } from "./rooms.service";
 
 export const router =new Hono();
 
-router.get("rooms/:roomId", (c) => {
+router.get("/:roomId", (c) => {
     const roomId = c.req.param("roomId");
     const room = roomsService.getBy(roomId);
     return c.json(room);
