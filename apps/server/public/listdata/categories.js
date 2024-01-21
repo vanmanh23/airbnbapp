9c0e9c13f405ce9f27029bdf909bdd2ebebab74e
manhnguyen <manhcan03@gmail.com>
2024-01-21 21:17:51 +0700

    add outDir for tsconfig file in server service

---
+++ apps/server/public/listdata/categories.js
@@ -0,0 +1,101 @@
+export class categoriesService {
+    static getAll() {
+        return [
+            {
+                id: "1",
+                name: "Rooms",
+                icon: "https://a0.muscache.com/pictures/3271df99-f071-4ecf-9128-eb2d2b1f50f0.jpg",
+            },
+            {
+                id: "2",
+                name: "Cabins",
+                icon: "	https://a0.muscache.com/pictures/732edad8-3ae0-49a8-a451-29a8010dcc0c.jpg",
+            },
+            {
+                id: "3",
+                name: "Beach",
+                icon: "https://a0.muscache.com/pictures/10ce1091-c854-40f3-a2fb-defc2995bcaf.jpg",
+            },
+            {
+                id: "4",
+                name: "A-frames",
+                icon: "https://a0.muscache.com/pictures/1d477273-96d6-4819-9bda-9085f809dad3.jpg",
+            },
+            {
+                id: "5",
+                name: "Castels",
+                icon: "https://a0.muscache.com/pictures/1b6a8b70-a3b6-48b5-88e1-2243d9172c06.jpg",
+            },
+            {
+                id: "6",
+                name: "Surfing",
+                icon: "	https://a0.muscache.com/pictures/957f8022-dfd7-426c-99fd-77ed792f6d7a.jpg",
+            },
+            {
+                id: "7",
+                name: "Amazing pools",
+                icon: "	https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg",
+            },
+            {
+                id: "8",
+                name: "Amazing views",
+                icon: "	https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
+            },
+            {
+                id: "9",
+                name: "chef's kitchens",
+                icon: "	https://a0.muscache.com/pictures/ddd13204-a5ae-4532-898c-2e595b1bb15f.jpg",
+            },
+            {
+                id: "10",
+                name: "Adapter",
+                icon: "https://a0.muscache.com/pictures/e22b0816-f0f3-42a0-a5db-e0f1fa93292b.jpg",
+            },
+            {
+                id: "11",
+                name: "Dammusi",
+                icon: "https://a0.muscache.com/pictures/c9157d0a-98fe-4516-af81-44022118fbc7.jpg",
+            },
+            {
+                id: "12",
+                name: "Campers",
+                icon: "https://a0.muscache.com/pictures/31c1d523-cc46-45b3-957a-da76c30c85f9.jpg",
+            },
+            {
+                id: "13",
+                name: "Islands",
+                icon: "	https://a0.muscache.com/pictures/8e507f16-4943-4be9-b707-59bd38d56309.jpg",
+            },
+            {
+                id: "14",
+                name: "National parks",
+                icon: "	https://a0.muscache.com/pictures/c0a24c04-ce1f-490c-833f-987613930eca.jpg",
+            },
+            {
+                id: "15",
+                name: "Cabins",
+                icon: "	https://a0.muscache.com/pictures/732edad8-3ae0-49a8-a451-29a8010dcc0c.jpg",
+            },
+            {
+                id: "16",
+                name: "caves",
+                icon: "	https://a0.muscache.com/pictures/4221e293-4770-4ea8-a4fa-9972158d4004.jpg",
+            },
+            {
+                id: "17",
+                name: "Golfing",
+                icon: "	https://a0.muscache.com/pictures/6b639c8d-cf9b-41fb-91a0-91af9d7677cc.jpg",
+            },
+            {
+                id: "18",
+                name: "bead & breakfasts",
+                icon: "https://a0.muscache.com/pictures/5ed8f7c7-2e1f-43a8-9a39-4edfc81a3325.jpg",
+            },
+            {
+                id: "19",
+                name: "New",
+                icon: "	https://a0.muscache.com/pictures/c0fa9598-4e37-40f3-b734-4bd0e2377add.jpg",
+            }
+        ];
+    }
+}
---
+++ apps/server/public/modules/rooms/rooms.service.js
@@ -0,0 +1,7 @@
+export class roomsService {
+    static getBy(roomId) {
+        if (roomId === "1") {
+            return [];
+        }
+    }
+}
