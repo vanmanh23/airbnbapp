41e67c436159ca9270476278c1a8a1edd6866a14
manhnguyen <manhcan03@gmail.com>
2024-01-24 17:02:38 +0700

    initial commit

--- apps/server/api/index.js
+++ apps/server/api/index.js
@@ -1,7 +1,1 @@
-// import { handle } from 'hono/vercel';
-// import app from '../src/app';
-// export const config = {
-//     runtime: "edge",
-// };
-// export default handle(app);
 export { default } from "../public/index.js";
