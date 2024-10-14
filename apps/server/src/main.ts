e5cf445b22da54bccbcd415d8478843519fa3b74
manhnguyen <manhcan03@gmail.com>
2024-10-14 21:57:45 +0700

    update version

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,6 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
   await app.listen(3000);
 }
