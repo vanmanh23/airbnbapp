2773a22c31785de2cdf6027d8a580d4b40f2f628
manhnguyen <manhcan03@gmail.com>
2024-10-17 10:57:19 +0700

    add database-url

--- apps/server/src/category/category.module.ts
+++ apps/server/src/category/category.module.ts
@@ -3,8 +3,8 @@ import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './category.entity';
-import { RoomsService } from 'src/rooms/rooms.service';
-import { Rooms } from 'src/rooms/rooms.entity';
+import { RoomsService } from '../../src/rooms/rooms.service';
+import { Rooms } from '../../src/rooms/rooms.entity';
 // import { RoomsModule } from 'src/rooms/rooms.module';
 
 @Module({
