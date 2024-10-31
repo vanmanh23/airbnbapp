66b5b2a42934bc065eb531ec521a99321013ebed
manhnguyen <manhcan03@gmail.com>
2024-10-31 22:32:22 +0700

    version update

--- apps/web/src/pages/404.tsx
+++ apps/web/src/pages/404.tsx
@@ -5,7 +5,7 @@ import Header from "@/components/Header";
 export default function Component() {
   return (
     <div>
-      <Header className="mx-auto w-full max-w-6xl" />
+      <Header hasSearch className="mx-auto w-full max-w-6xl" />
       <hr />
       <div className="mx-auto max-w-6xl">
         We can’t seem to find the page you’re looking for Here are some helpful links instead: Home Traveling on Airbnb
